27e02205a2327cb6ff6ab26f24dff8f7e42d5066
ivoorusso <ivan@setandforget.io>
2024-09-17 16:56:46 -0300

    update filters style

--- src/app/page.js
+++ src/app/page.js
@@ -174,7 +174,7 @@ export default function Home() {
 
 
           <div className="bg-white mb-8 p-6 rounded-lg shadow-lg w-full">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
 
               {/* Front/Back Filter */}
               <FormControl variant="outlined" className="w-full">
