5bbdda752151305b653bfddbcdfe351af85180d2
ivoorusso <ivan@setandforget.io>
2024-09-17 11:53:58 -0300

    styles updates

--- src/app/page.js
+++ src/app/page.js
@@ -77,136 +77,78 @@ export default function Home() {
 
 
   const renderBoxes = (phase1, phase2, phase3, prefix) => {
-    return [...Array(3)].map((_, rowIndex) => (
-      <div key={rowIndex} className="flex gap-1 mb-1">
-        <div className="flex gap-1 border-r border-yellow-600 pr-1">
-          {phase1.slice(rowIndex * 5, rowIndex * 5 + 5).map((boxIndex) => {
-            const disabled = !filterMatch(boxIndex, prefix);
-            const phase = getPhase(boxIndex);
-            return (
-              <Tooltip
-                key={prefix + boxIndex}
-                title={
-                  <div className="text-center p-2">
-                    <p className="text-sm font-semibold text-gray-700">Box {boxIndex}</p>
-                    <p className="text-xs text-gray-500">{phase}</p>
-                    <p className="text-xs text-gray-500">{prefix === 'F' ? 'Front' : 'Back'}</p>
-                  </div>
-                }
-                arrow
-                classes={{
-                  tooltip: 'custom-tooltip',
-                  arrow: 'custom-tooltip-arrow',
-                }}
-              >
-                <div
-                  className={`w-10 h-10 rounded-md ${selectedBox === prefix + boxIndex
-                    ? 'bg-orange-500'
-                    : disabled
-                      ? 'bg-gray-300 cursor-not-allowed'
-                      : 'bg-yellow-100 border-yellow-600 cursor-pointer hover:bg-yellow-200 hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out'
-                    } border flex items-center justify-center`}
-                  onClick={() => handleBoxClick(prefix + boxIndex, disabled)}
-                >
-                  <span className="text-black font-bold">{boxIndex}</span>
+    const allBoxes = [...phase1, ...phase2, ...phase3];
+
+    const getBoxColor = (boxIndex) => {
+      if (boxIndex <= 15) return { color: 'yellow-100', borderColor: 'yellow-600' };
+      if (boxIndex <= 30) return { color: 'green-100', borderColor: 'green-600' };
+      return { color: 'teal-100', borderColor: 'teal-600' };
+    };
+
+    return (
+      <div className="flex gap-2 pr-4 overflow-x-auto">
+        {allBoxes.map((boxIndex) => {
+          const disabled = !filterMatch(boxIndex, prefix);
+          const phase = getPhase(boxIndex);
+          const { color, borderColor } = getBoxColor(boxIndex);
+
+          return (
+            <Tooltip
+              key={prefix + boxIndex}
+              title={
+                <div className="text-center p-2">
+                  <p className="text-sm font-semibold text-gray-700">Box {boxIndex}</p>
+                  <p className="text-xs text-gray-500">{phase}</p>
+                  <p className="text-xs text-gray-500">{prefix === 'F' ? 'Front' : 'Back'}</p>
                 </div>
-              </Tooltip>
-            );
-          })}
-        </div>
-        <div className="flex gap-1 border-r border-green-600 pr-1">
-          {phase2.slice(rowIndex * 5, rowIndex * 5 + 5).map((boxIndex) => {
-            const disabled = !filterMatch(boxIndex, prefix);
-            const phase = getPhase(boxIndex);
-            return (
-              <Tooltip
-                key={prefix + boxIndex}
-                title={
-                  <div className="text-center p-2">
-                    <p className="text-sm font-semibold text-gray-700">Box {boxIndex}</p>
-                    <p className="text-xs text-gray-500">{phase}</p>
-                    <p className="text-xs text-gray-500">{prefix === 'F' ? 'Front' : 'Back'}</p>
-                  </div>
-                }
-                arrow
-                classes={{
-                  tooltip: 'custom-tooltip',
-                  arrow: 'custom-tooltip-arrow',
-                }}
+              }
+              arrow
+              classes={{
+                tooltip: 'custom-tooltip',
+                arrow: 'custom-tooltip-arrow',
+              }}
+            >
+              <div
+                className={`w-16 h-16 rounded-md ${selectedBox === prefix + boxIndex
+                  ? 'bg-orange-500'
+                  : disabled
+                    ? 'bg-gray-300 cursor-not-allowed'
+                    : `bg-${color} border-${borderColor} cursor-pointer hover:bg-yellow-200 hover:shadow-lg transform transition-all duration-300 ease-in-out`
+                  } border flex items-center justify-center`}
+                onClick={() => handleBoxClick(prefix + boxIndex, disabled)}
               >
-                <div
-                  className={`w-10 h-10 rounded-md ${selectedBox === prefix + boxIndex
-                    ? 'bg-orange-500'
-                    : disabled
-                      ? 'bg-gray-300 cursor-not-allowed'
-                      : 'bg-green-100 border-green-600 cursor-pointer hover:bg-green-200 hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out'
-                    } border flex items-center justify-center`}
-                  onClick={() => handleBoxClick(prefix + boxIndex, disabled)}
-                >
-                  <span className="text-black font-bold">{boxIndex}</span>
-                </div>
-              </Tooltip>
-            );
-          })}
-        </div>
-        <div className="flex gap-1">
-          {phase3.slice(rowIndex * 6, rowIndex * 6 + 6).map((boxIndex) => {
-            const disabled = !filterMatch(boxIndex, prefix);
-            const phase = getPhase(boxIndex);
-            return (
-              <Tooltip
-                key={prefix + boxIndex}
-                title={
-                  <div className="text-center p-2">
-                    <p className="text-sm font-semibold text-gray-700">Box {boxIndex}</p>
-                    <p className="text-xs text-gray-500">{phase}</p>
-                    <p className="text-xs text-gray-500">{prefix === 'F' ? 'Front' : 'Back'}</p>
-                  </div>
-                }
-                arrow
-                classes={{
-                  tooltip: 'custom-tooltip',
-                  arrow: 'custom-tooltip-arrow',
-                }}
-              >
-                <div
-                  className={`w-10 h-10 rounded-md ${selectedBox === prefix + boxIndex
-                    ? 'bg-orange-500'
-                    : disabled
-                      ? 'bg-gray-300 cursor-not-allowed'
-                      : 'bg-teal-100 border-teal-600 cursor-pointer hover:bg-teal-200 hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out'
-                    } border flex items-center justify-center`}
-                  onClick={() => handleBoxClick(prefix + boxIndex, disabled)}
-                >
-                  <span className="text-black font-bold">{boxIndex}</span>
-                </div>
-              </Tooltip>
-            );
-          })}
-        </div>
+                <span className="text-black font-bold">{boxIndex}</span>
+              </div>
+            </Tooltip>
+          );
+        })}
       </div>
-    ));
+    );
   };
 
 
 
 
+
   return (
-    <main className="flex h-screen overflow-hidden">
-      {/* Header con animación de colapsado */}
+    <main className="flex flex-col h-screen relative">
+      {/* Header con animación de colapsado y superpuesto al contenido */}
       <aside
-        className={`menu-container ${isHeaderCollapsed ? '-translate-x-full' : 'translate-x-0'}`}
+        className={`menu-container fixed top-0 left-0 h-full bg-white shadow-lg z-30 transition-transform duration-300 ease-in-out ${isHeaderCollapsed ? '-translate-x-full' : 'translate-x-0'}`}
+        style={{ width: '250px' }} 
       >
-        <IconButton
-          onClick={() => setHeaderCollapsed(!isHeaderCollapsed)}
-          className="menu-icon-button"
-        >
-          <MenuIcon style={{ fontSize: '36px' }} />
-        </IconButton>
         {!isHeaderCollapsed && (
           <>
-            <div className="menu-logo"></div>
-            <div className="flex flex-col items-center space-y-4">
+            {/* Button over the logo to close the menu */}
+            <IconButton
+              onClick={() => setHeaderCollapsed(true)} 
+              className="absolute left-4 top-4 bg-white rounded-full p-2 shadow-lg z-50" 
+            >
+              <MenuIcon style={{ fontSize: '36px', color: 'black' }} /> 
+            </IconButton>
+
+            <div className="menu-logo p-4"></div>
+            <div className="flex flex-col items-center space-y-4 p-4">
               <div className="menu-item"></div>
               <div className="menu-item"></div>
             </div>
@@ -214,23 +156,22 @@ export default function Home() {
         )}
       </aside>
 
-      {/* Botón flotante para abrir el menú cuando está colapsado */}
+      {/* Floating button to open the menu when collapsed */}
       {isHeaderCollapsed && (
         <IconButton
-          onClick={() => setHeaderCollapsed(false)}
-          className="menu-fixed-icon-button"
+          onClick={() => setHeaderCollapsed(false)} 
+          className="fixed top-4 left-4 z-40 bg-white p-2 rounded-full shadow-lg menu-fixed-icon-button"
         >
           <MenuIcon style={{ fontSize: '36px' }} />
         </IconButton>
       )}
 
-      {/* Contenido principal centrado y ajustado */}
-      <section className="flex-1 p-8 flex flex-col items-center justify-start mt-8">
-        <div className="flex flex-col w-full max-w-4xl">
-          {/* Filtros */}
+
+      <section className="flex-1 p-4 flex flex-col items-center justify-start overflow-auto relative mt-24"> 
+        <div className="flex flex-col w-full max-w-full lg:max-w-7xl">
+
           <div className="bg-white mb-8 p-6 rounded-lg shadow-lg w-full">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {/* Front/Back Filter */}
               <FormControl variant="outlined" className="w-full">
                 <InputLabel>Front/Back</InputLabel>
                 <Select
@@ -307,7 +248,6 @@ export default function Home() {
                 type="number"
               />
 
-
               {/* Locations with product/Empty Locations Filter */}
               <FormControlLabel
                 control={
@@ -322,32 +262,37 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Boxes */}
-          <div className="bg-gray-100 p-6 rounded-lg shadow-lg flex-1 w-full overflow-auto">
+          {/* Boxes Container */}
+          <div className="bg-gray-100 p-8 rounded-lg shadow-lg w-full lg:mt-8 overflow-x-auto"> 
             <div className="mb-6">
               <div className="flex items-center mb-4">
-                <div className="text-left text-2xl font-bold mr-4 text-gray-700 tracking-wide uppercase" style={{ minWidth: "100px", letterSpacing: "0.1em" }}>
+                {/* Fixed width for Front label */}
+                <div className="text-left text-2xl font-bold mr-4 text-gray-700 uppercase tracking-wide" style={{ minWidth: "120px" }}>
                   Front
-                </div>                <div className="flex flex-col">
+                </div>
+                <div className="flex flex-wrap gap-4">
                   {renderBoxes(
-                    Array.from({ length: 15 }, (_, i) => i + 1),        // Phase 1: 1-15
-                    Array.from({ length: 15 }, (_, i) => i + 16),       // Phase 2: 16-30
-                    Array.from({ length: 18 }, (_, i) => i + 31),       // Phase 3: 31-48
-                    "F"  // Prefijo para las boxes de "Front"
+                    Array.from({ length: 15 }, (_, i) => i + 1),
+                    Array.from({ length: 15 }, (_, i) => i + 16),
+                    Array.from({ length: 18 }, (_, i) => i + 31),
+                    "F"
                   )}
                 </div>
               </div>
             </div>
+
             <div>
               <div className="flex items-center mb-4">
-                <div className="text-left text-2xl font-bold mr-4 text-gray-700 tracking-wide uppercase" style={{ minWidth: "100px", letterSpacing: "0.1em" }}>
+                {/* Fixed width for Back label */}
+                <div className="text-left text-2xl font-bold mr-4 text-gray-700 uppercase tracking-wide" style={{ minWidth: "120px" }}>
                   Back
-                </div>                <div className="flex flex-col">
+                </div>
+                <div className="flex flex-wrap gap-4">
                   {renderBoxes(
-                    Array.from({ length: 15 }, (_, i) => i + 1),        // Phase 1: 1-15
-                    Array.from({ length: 15 }, (_, i) => i + 16),       // Phase 2: 16-30
-                    Array.from({ length: 18 }, (_, i) => i + 31),       // Phase 3: 31-48
-                    "B"  // Prefijo para las boxes de "Back"
+                    Array.from({ length: 15 }, (_, i) => i + 1),
+                    Array.from({ length: 15 }, (_, i) => i + 16),
+                    Array.from({ length: 18 }, (_, i) => i + 31),
+                    "B"
                   )}
                 </div>
               </div>
@@ -356,7 +301,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Menú derecho - Usando Drawer */}
+      {/* Menú derecho */}
       <Drawer
         anchor="right"
         open={isDrawerOpen}
@@ -377,5 +322,9 @@ export default function Home() {
         </Box>
       </Drawer>
     </main>
+
+
   );
 }
+
+
